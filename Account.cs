8718845d5cb9354a58f486f5572eaadad25fdc7f
Samara Gama <samaragamaferreirads@gmail.com>
2021-02-22 22:27:15 -0300

    Initial commit

---
+++ Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBank
+{
+    class Account
+    {
+        public string owner { get; set; }
+        public int  branch { get; set; }
+        public int account { get; set; }
+        public double balance { get; set; }
+
+        //public Account(string owner, int branch, int account, double balance)
+        //{
+
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account();
+
+            account1.owner = "Samara";
+            account1.branch = 1234;
+            account1.account = 12341;
+            account1.balance = 50.0;
+
+            Console.WriteLine($"Account owner: {account1.owner}. Branch: {account1.branch}. Account id: {account1.account}. Balance: ${account1.balance}.");
+            Console.ReadLine();
+
+        }
+    }
+}
