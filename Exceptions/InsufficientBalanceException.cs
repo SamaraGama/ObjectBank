6fe7ee378af2806b04556c1cabdd42879b7a7729
samaraGama <samaragamaferreirads@gmail.com>
2021-04-21 01:13:44 -0300

    Create InsufficientBalanceException Class :sparkles:

---
+++ Exceptions/InsufficientBalanceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBank.Exceptions
+{
+    class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException()
+        {
+                
+        }
+        public InsufficientBalanceException(string message) : base(message)
+        {
+
+        }
+    }
+}
